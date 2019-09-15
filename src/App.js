950345310f909a471f4e10ffe3ad95e1ce189bb3
Swastik Panda <swastiksuman@gmail.com>
2019-09-15 21:12:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Hero(){
+  return (<div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>AuthorQuiz</h1>
+      <p>Select the book written by the Author shown</p>
+    </div>
+  </div>);
+}
+
+function Turn({author, books}){
+  return (<div className="row turn" style={{backgroundColor: "white"}}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author"></img>
+      </div>
+      <div className="col-6">
+        {books.map((title) => <p>{title}</p>)}
+      </div>
     </div>
   );
 }
 
-export default App;
+function Continue(){
+  return <h1>s</h1>;
+}
+
+function Footer(){
+  return <h1>Footer</h1>;
+}
+
+function AuthorQuiz({turnData}) {
+  return (
+    <div className="container-fluid">
+      <Hero/>
+      <Turn {...turnData}/>
+      <Continue/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AuthorQuiz from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const authors = [
+    {
+        name: "Mark Twain",
+        imageUrl: "https://cdn.britannica.com/s:300x300/83/136283-050-9C9D6ED8/Mark-Twain-1907.jpg",
+        imageSource: "Wikimedia Commons",
+        books: ["The Adventures of Huckleberry Finn"]
+    }
+];
+
+const state = {
+    turnData: {
+        author: authors[0],
+        books: authors[0].books
+    }
+}
+
+ReactDOM.render(<AuthorQuiz {...state}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
